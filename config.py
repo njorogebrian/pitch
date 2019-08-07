5d40e53aa2eddbafc10cddc4ecf2f92bc811dfe7
njorogebrian <briannjoroge2000@gmail.com>
2019-08-07 17:29:41 +0300

    create navbar

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mark:aguero10@localhost/pitchapp'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brian:ps4@localhost/pitchapp'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
@@ -13,8 +13,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME") 
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME =("MAIL_USERNAME") 
+    MAIL_PASSWORD =("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
@@ -23,7 +23,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent config class with general config classes
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brian:ps4@localhost/pitchapp'
 
 class TestConfig(Config):
     '''
@@ -32,7 +32,7 @@ class TestConfig(Config):
     Args:
         Config: The parent config class with General config settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mark:aguero10@localhost/pitchapp'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brian:ps4@localhost/pitchapp'
 
 class DevConfig(Config):
     '''
@@ -41,8 +41,7 @@ class DevConfig(Config):
     Args:
         Config: The parent config class with general config settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mark:aguero10@localhost/pitchapp'
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brian:ps4@localhost/pitchapp'
     DEBUG = True
 
 config_options = {
